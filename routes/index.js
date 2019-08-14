b38ee8877075e0759daaef869071d9ebe130dc82
azheng4119 <azheng4119@bths.edu>
2019-08-14 14:51:38 -0400

    heroku deploy

--- routes/index.js
+++ routes/index.js
@@ -6,9 +6,7 @@ router.use("/stations", stationsRouter);
 router.use("/trains", trainsRouter);
 
 router.use((req, res, next) => {
-    const error = new Error("Not Found, Please Check URL!");
-    error.status = 404;
-    next(error);
+    res.status(404).send("Not Found, Please Check URL!");
   });
 
   module.exports = router;
