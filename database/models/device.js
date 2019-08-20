224a8edad89324780a9149b2e137cd47712ff394
Toms <chen.tommy925@gmail.com>
2019-08-20 12:14:03 -0400

    added device

---
+++ database/models/device.js
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+mongoose.set('useCreateIndex', true);
+mongoose.set('useFindAndModify', false);
+
+const deviceSchema = mongoose.Schema({
+    deviceid: {
+        type: String,
+        unique: true,
+        required: true
+    },
+    stations: {
+        type:[String],
+        default:undefined
+    },
+})
+
+const Device = mongoose.model('device', deviceSchema)
+module.exports = Device;
